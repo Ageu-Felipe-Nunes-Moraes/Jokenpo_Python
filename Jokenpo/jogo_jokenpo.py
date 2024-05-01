7df9064781440eef0a4679c9a777bdbec31f2dd1
Ageu Moraes <ageumoraes67@gmail.com>
2024-05-01 11:47:21 -0300

    Otimizando acessibilidade com variáveis em inglês

--- Jokenpo/jogo_jokenpo.py
+++ Jokenpo/jogo_jokenpo.py
@@ -5,102 +5,102 @@ import random as r
 class Jokenpo:
 
     def __init__(self):
-        self.cria_linha_dupla()
+        self.creates_double_lines()
         print("JOKENPÔ - CONTRA O COMPUTADOR")
-        self.cria_linha_dupla()
-        self.escolha_maquina = ""
-        self.escolha_pessoa = 0
-        self.lista_opcoes = ["PEDRA", "PAPEL", "TESOURA"]
+        self.creates_double_lines()
+        self.machine_choice = ""
+        self.person_choice = 0
+        self.options_list = ["PEDRA", "PAPEL", "TESOURA"]
 
 
-    def tempo_delay(self):
+    def delay_time(self):
         print("COMPUTADOR PENSANDO...")
-        lista_quantidade_pontos = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-        quantidade_escolhida = r.choice(lista_quantidade_pontos)
-        for _ in range(quantidade_escolhida):
-            lista_segundos = [0.1, 0.2, 0.3, 0.4, 0.5]
-            segundo_escolhido = r.choice(lista_segundos)
-            t.sleep(segundo_escolhido)
+        list_points_quantity = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+        quantity_chosen = r.choice(list_points_quantity)
+        for _ in range(quantity_chosen):
+            seconds_list = [0.1, 0.2, 0.3, 0.4, 0.5]
+            second_chosen = r.choice(seconds_list)
+            t.sleep(second_chosen)
             print(".")
-            segundo_escolhido = r.choice(lista_segundos)
-            t.sleep(segundo_escolhido)
-        self.cria_linha_dupla()
+            second_chosen = r.choice(seconds_list)
+            t.sleep(second_chosen)
+        self.creates_double_lines()
 
 
-    def cria_linha_dupla(self):
+    def creates_double_lines(self):
         print("=" * 60)
 
 
-    def cria_linha_simples(self):
+    def creates_simple_line(self):
         print("-" * 60)
 
 
-    def introducao(self):
+    def introduction(self):
         print("INSTRUCÕES DE COMO JOGAR:")
-        self.cria_linha_simples()
+        self.creates_simple_line()
         print("1--PEDRA")
         print("2--PAPEL")
         print("3--TESOURA")
         print("0--SAIR")
-        self.cria_linha_simples()
+        self.creates_simple_line()
 
 
-    def valor_escolha_computador(self):
-        self.escolha_maquina = r.choice(self.lista_opcoes)
-        return self.escolha_maquina
+    def computer_choice_value(self):
+        self.machine_choice = r.choice(self.options_list)
+        return self.machine_choice
     
 
-    def valor_escolha_pessoa(self):
-        self.escolha_pessoa = int(input("DIGITE O NÚMERO DA SUA ESCOLHA: "))
-        self.cria_linha_simples()
-        return self.escolha_pessoa
+    def person_choice_value(self):
+        self.person_choice = int(input("DIGITE O NÚMERO DA SUA ESCOLHA: "))
+        self.creates_simple_line()
+        return self.person_choice
     
 
-    def mostra_escolhas(self):
-        variavel_controle = True
+    def show_choices(self):
+        control_variable = True
 
         
-        while variavel_controle:
-            self.escolha_pessoa = self.valor_escolha_pessoa()
-            if self.escolha_pessoa == 0:
+        while control_variable:
+            self.person_choice = self.person_choice_value()
+            if self.person_choice == 0:
                 print("FIM DE JOGO!!!")
-                variavel_controle = False
+                control_variable = False
 
-            elif self.escolha_pessoa > 0 and self.escolha_pessoa < 4:
-                self.tempo_delay()
-                self.escolha_maquina = self.valor_escolha_computador()
-                print(f"HUMANO(VOCÊ): {self.lista_opcoes[self.escolha_pessoa - 1]}")
-                print(f"COMPUTADOR: {self.escolha_maquina}")
-                self.cria_linha_simples()
-                self.verifica_vencedor()
-                variavel_controle = False
+            elif self.person_choice > 0 and self.person_choice < 4:
+                self.delay_time()
+                self.machine_choice = self.computer_choice_value()
+                print(f"HUMANO(VOCÊ): {self.options_list[self.person_choice - 1]}")
+                print(f"COMPUTADOR: {self.machine_choice}")
+                self.creates_simple_line()
+                self.check_winner()
+                control_variable = False
 
             else:
                 print("VALOR INVÁLIDO!!! TENTE NOVAMENTE!!")
-                self.cria_linha_dupla()
+                self.creates_double_lines()
 
 
-    def verifica_vencedor(self):
-        if self.lista_opcoes[self.escolha_pessoa - 1] == self.escolha_maquina:
+    def check_winner(self):
+        if self.options_list[self.person_choice - 1] == self.machine_choice:
             print("EMPATE!!!")
 
         else:
                 
-            if self.lista_opcoes[self.escolha_pessoa - 1] == "PEDRA" and self.escolha_maquina == "TESOURA" or self.lista_opcoes[self.escolha_pessoa - 1] == "PAPEL" and self.escolha_maquina == "PEDRA" or self.lista_opcoes[self.escolha_pessoa - 1] == "TESOURA" and self.escolha_maquina == "PAPEL":
+            if self.options_list[self.person_choice - 1] == "PEDRA" and self.machine_choice == "TESOURA" or self.options_list[self.person_choice - 1] == "PAPEL" and self.machine_choice == "PEDRA" or self.options_list[self.person_choice - 1] == "TESOURA" and self.machine_choice == "PAPEL":
 
                 print("VOCÊ VENCEU!!!")
 
-            elif self.escolha_pessoa == 0:
+            elif self.person_choice == 0:
                 print("")
                 
             else:
                 print("VOCÊ PERDEU")
 
-        if self.escolha_pessoa != 0:
-            self.cria_linha_dupla()
-            self.mostra_escolhas()
+        if self.person_choice != 0:
+            self.creates_double_lines()
+            self.show_choices()
 
 
-iniciar_jogo = Jokenpo()
-iniciar_jogo.introducao()
-iniciar_jogo.mostra_escolhas()
+start_Game = Jokenpo()
+start_Game.introduction()
+start_Game.show_choices()
